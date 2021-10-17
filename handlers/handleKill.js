ce3f9ea67f92cbdddf77520cb15f1240f7e1fa33
SteveKanger <hi.stevekanger@gmail.com>
2021-10-17 11:07:16 -0500

    Added kill items

--- handlers/handleKill.js
+++ handlers/handleKill.js
@@ -1,9 +1,10 @@
 const handleKill = (controllerVars, resize) => {
-  const { listeners, container, scrollbar } = controllerVars.get()
+  const { listeners, container, scrollbar, items } = controllerVars.get()
 
   if (scrollbar) scrollbar.kill()
   container.kill()
   listeners.kill()
+  items.forEach((item) => item.kill())
   window.removeEventListener('resize', resize)
 }
 
